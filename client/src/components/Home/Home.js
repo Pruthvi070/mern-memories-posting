822e95443c2abb6a262e095f7e8a96902d100842
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2024-08-29 10:37:17 +0530

    Ui change

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -48,13 +48,13 @@ const Home = () => {
 
   return (
     <Grow in>
-      <Container maxWidth="xl" className={classes.containerBackground}>
-        <Grid container justifyContent="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+      <Container maxWidth="xl">
+        <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
           <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <AppBar className={classes.appBarSearch} position="static" color="transparent">
+            <AppBar className={classes.appBarSearch} position="static" color="inherit">
               <TextField onKeyDown={handleKeyPress} name="search" variant="outlined" label="Search Memories" fullWidth value={search} onChange={(e) => setSearch(e.target.value)} />
               <ChipInput
                 style={{ margin: '10px 0' }}
