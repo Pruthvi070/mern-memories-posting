83973fa2a867f0ca942e6c338ccef847cfb2c261
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2024-08-30 19:00:41 +0530

    .env created

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
+
+dotenv.config(); // Load environment variables
 
 const app = express();
 
@@ -15,8 +18,8 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://Raj:21512757@newmern.t70lv.mongodb.net/?retryWrites=true&w=majority&appName=Newmern';
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
