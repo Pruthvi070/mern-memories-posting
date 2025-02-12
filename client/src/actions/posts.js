e9147ab407615dbbde1b18e7006d544925c3964a
Pruthviraj Chavan <chavanpruthviraj9595@gmail.com>
2025-02-12 11:27:31 +0530

    implement profile page functionality

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,107 +1,24 @@
-import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE, COMMENT, FETCH_BY_CREATOR } from '../constants/actionTypes';
-import * as api from '../api/index.js';
+import axios from 'axios';
 
-export const getPost = (id) => async (dispatch) => {
-  try {
-    dispatch({ type: START_LOADING });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
-    const { data } = await api.fetchPost(id);
-
-    dispatch({ type: FETCH_POST, payload: { post: data } });
-  } catch (error) {
-    console.log(error);
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
   }
-};
 
-export const getPosts = (page) => async (dispatch) => {
-  try {
-    dispatch({ type: START_LOADING });
-    const { data: { data, currentPage, numberOfPages } } = await api.fetchPosts(page);
+  return req;
+});
 
-    dispatch({ type: FETCH_ALL, payload: { data, currentPage, numberOfPages } });
-    dispatch({ type: END_LOADING });
-  } catch (error) {
-    console.log(error);
-  }
-};
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
+export const fetchPostsByCreator = (name) => API.get(`/posts/creator?name=${name}`);
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+export const comment = (value, id) => API.post(`/posts/${id}/commentPost`, { value });
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
 
-export const getPostsByCreator = (name) => async (dispatch) => {
-  try {
-    dispatch({ type: START_LOADING });
-    const { data: { data } } = await api.fetchPostsByCreator(name);
-
-    dispatch({ type: FETCH_BY_CREATOR, payload: { data } });
-    dispatch({ type: END_LOADING });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const getPostsBySearch = (searchQuery) => async (dispatch) => {
-  try {
-    dispatch({ type: START_LOADING });
-    const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
-
-    dispatch({ type: FETCH_BY_SEARCH, payload: { data } });
-    dispatch({ type: END_LOADING });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const createPost = (post, history) => async (dispatch) => {
-  try {
-    dispatch({ type: START_LOADING });
-    const { data } = await api.createPost(post);
-
-    dispatch({ type: CREATE, payload: data });
-
-    history.push(`/posts/${data._id}`);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const updatePost = (id, post) => async (dispatch) => {
-  try {
-    const { data } = await api.updatePost(id, post);
-
-    dispatch({ type: UPDATE, payload: data });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const likePost = (id) => async (dispatch) => {
-  const user = JSON.parse(localStorage.getItem('profile'));
-
-  try {
-    const { data } = await api.likePost(id, user?.token);
-
-    dispatch({ type: LIKE, payload: data });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const commentPost = (value, id) => async (dispatch) => {
-  try {
-    const { data } = await api.comment(value, id);
-
-    dispatch({ type: COMMENT, payload: data });
-
-    return data.comments;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const deletePost = (id) => async (dispatch) => {
-  try {
-    await await api.deletePost(id);
-
-    dispatch({ type: DELETE, payload: id });
-  } catch (error) {
-    console.log(error);
-  }
-};
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
